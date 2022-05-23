8b9ac5160266f93ec530654abeb7f5960f42d2ba
esanchep <esanchezp85@gmail.com>
2022-05-24 00:32:11 +0200

    Update key references

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -13,12 +13,12 @@ def on_complete(stream, file_path):
     pass
 
 layout = [
-    [gui.Text('URL:'), gui.Input(key = '-URL-', expand_x = True), gui.Button('Search', key = 'search')], # URL Input + search button
+    [gui.Text('URL:'), gui.Input(key = '-URL-', expand_x = True), gui.Button('Search', key = '-SEARCH-')], # URL Input + search button
     [gui.Text('Title: '), gui.Text('', key = '-TITLE-')], # Video title
     [gui.Text('Length: '), gui.Text('', key = '-LENGTH-')], # Length in minutes
     [gui.Text('Audio size: '), gui.Text('', key = '-SIZE-')], # Size in MB
     [gui.Text('Download directory: '), gui.Input(key = '-OUTPUTPATH-'), gui.FolderBrowse()],
-    [gui.Button('Download', key = 'download'), gui.Progress(100, size = (20, 20), expand_x = True, key = '-PROGRESSBAR-')] # download button + progress bar
+    [gui.Button('Download', key = '-DOWNLOAD-'), gui.Progress(100, size = (20, 20), expand_x = True, key = '-PROGRESSBAR-')] # download button + progress bar
 ]
 
 window = gui.Window('YouTube Downloader', layout)
@@ -29,7 +29,7 @@ while True:
     if event == gui.WIN_CLOSED:
         break
 
-    if event == 'search':
+    if event == '-SEARCH-':
         restart_progress_bar()
         if not values['-URL-']:
             continue
@@ -38,7 +38,7 @@ while True:
         window['-LENGTH-'].update(f'{round(youtube_video_info.length / 60, 2) } minutes')
         window['-SIZE-'].update(f'{round(youtube_video_info.streams.get_audio_only().filesize / 1048576, 1) } MB')
 
-    if event == 'download':
+    if event == '-DOWNLOAD-':
         restart_progress_bar()
         downloaded_audio_file = youtube_video_info.streams.get_audio_only().download(output_path = values['-OUTPUTPATH-'])
         base, ext = os.path.splitext(downloaded_audio_file)
